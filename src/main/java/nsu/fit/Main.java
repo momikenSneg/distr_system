5850a93d3e9e9d6bdb68e9db30715723bcbcbd1d
Kate <momiken98@gmail.com>
2020-05-04 23:55:02 +0700

    Apache Commons Compress seems work

--- src/main/java/nsu/fit/Main.java
+++ src/main/java/nsu/fit/Main.java
@@ -1,9 +1,11 @@
 package nsu.fit;
+
+import org.apache.commons.compress.compressors.CompressorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws CompressorException {
         Logger logger = LoggerFactory.getLogger(Main.class);
         logger.info("Hello World");
     }
