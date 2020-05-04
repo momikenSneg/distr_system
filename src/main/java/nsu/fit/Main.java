be920346741711a20c997ebdb8d00b847a81f950
Kate <momiken98@gmail.com>
2020-05-04 21:13:23 +0700

    first commit

---
+++ src/main/java/nsu/fit/Main.java
@@ -0,0 +1,10 @@
+package nsu.fit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args){
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Hello World");
+    }
+}
