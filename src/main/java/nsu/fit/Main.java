40867297868cde1e813068db76138cc41044ad10
Kate <momiken98@gmail.com>
2020-05-05 18:21:44 +0700

    + cli

--- src/main/java/nsu/fit/Main.java
+++ src/main/java/nsu/fit/Main.java
@@ -1,12 +1,16 @@
 package nsu.fit;
 
-import org.apache.commons.compress.compressors.CompressorException;
+import org.apache.commons.cli.Options;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-    public static void main(String[] args) throws CompressorException {
+    public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(Main.class);
         logger.info("Hello World");
+
+        Options options = new Options();
+
+        options.addOption("t", false, "display current time");
     }
 }
